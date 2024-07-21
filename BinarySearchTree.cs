f8ac4ea55dfda8ecf4eb2e6c9eccc14b38ea593d
ertugakmann <134059022+ertugakmann@users.noreply.github.com>
2024-07-21 22:49:02 +0300

    Proje dosyası ekle.

---
+++ BinarySearchTree.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSTTrees
+{
+    class BinarySearchTree
+    {
+        private Node root;
+
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+        public BinarySearchTree(int data)
+        {
+            root = new Node(data);            
+        }
+
+        public bool InsertNode(int data)
+        {
+            Node newNode = new Node(data);
+
+            if(root == null)
+            {
+                root = newNode;
+                return true;
+            }
+            else
+            {
+                Node temp = root;
+
+                while (true)
+                {
+                    if (newNode.data == temp.data)
+                        return false;
+
+                    if(newNode.data < temp.data)
+                    {
+                        if (temp.left == null)
+                        {
+                            temp.left = newNode;
+                            return true;
+                        }
+                        temp.left = temp;
+                    }
+                    else
+                    {
+                        if(temp.right == null)
+                        {
+                            temp.right = newNode;
+                            return true;
+                        }
+                        temp.right = temp;
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSTTrees
+{
+    class Node
+    {
+        public int data { get; set; }
+        public Node right { get; set; }
+
+        public Node left { get; set; }
+
+        public Node(int data)
+        {
+            this.data = data;
+
+            right = null;
+            left = null;
+        }
+    }
+}
